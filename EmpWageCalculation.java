2782efd680cf0fb915e184667629558e15153b60
Daminichandrakar <damini.2201@gmail.com>
2021-08-13 00:59:14 +0530

    Added:EmpAttandance Calculation

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -3,7 +3,12 @@ package com.bl.Emp;
 public class EmpWageCalculation {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to empWage");
+		 int IS_FULL_TIME=1;
+         double empCheck=Math.floor(Math.random()*10)%2;
+         if(empCheck==IS_FULL_TIME)
+                 System.out.println("Employee is Present");
+         else
+                 System.out.println("Employee is Absent");
 
 	}
 
