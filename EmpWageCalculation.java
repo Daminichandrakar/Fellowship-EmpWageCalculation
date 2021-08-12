ae683da295e280aeb04a7f3145c18006496ed5e2
Daminichandrakar <damini.2201@gmail.com>
2021-08-13 01:01:47 +0530

    Daily Emp Wage

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -2,14 +2,19 @@ package com.bl.Emp;
 
 public class EmpWageCalculation {
 
-	public static void main(String[] args) {
-		 int IS_FULL_TIME=1;
-         double empCheck=Math.floor(Math.random()*10)%2;
-         if(empCheck==IS_FULL_TIME)
-                 System.out.println("Employee is Present");
-         else
-                 System.out.println("Employee is Absent");
-
-	}
+	 static final int IS_FULL_TIME=1;
+     static final int EMP_RATE_PER_HOUR=20;
+     public static void main(String[] args)
+     {
+		int empHrs=0;
+		int empWage=0;
+             double empCheck=Math.floor(Math.random()*10)%2;
+             if(empCheck==IS_FULL_TIME)
+                     empHrs=8;
+             else
+                     empHrs=0;
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: "+empWage);
+     }
 
 }
