fecc0ea6a62bc5beb4867511a35287a7ace349a3
Daminichandrakar <damini.2201@gmail.com>
2021-08-13 01:17:11 +0530

    PartTime Emp Wage

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -1,34 +1,21 @@
 package com.bl.Emp;
 
 public class EmpWageCalculation {
-	public static final int IS_FULL_TIME=2;
-	public static final int IS_PART_TIME=1;
-        public static final int EMP_RATE_PER_HOUR=20;
-	public static final int NUM_OF_WORKING_DAYS=20;
-	public static final int MAX_HRS_IN_MONTH=100;
-        public static void main(String[] args)
-        {
-		int empWage=0,totalEmpWage=0,empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
-		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
-		{
-			totalWorkingDays++;
-                	double empCheck=Math.floor(Math.random()*10)%3;
-			switch((int)empCheck)
-			{
-				case IS_FULL_TIME:
-					empHrs=8;
-					break;
-				case IS_PART_TIME:
-					empHrs=4;
-					break;
-				default:
-					empHrs=0;
-					break;
-                	}
-			totalEmpHrs+=empHrs;
+
+	 static final int IS_FULL_TIME=2;
+		static final int IS_PART_TIME=1;
+	        static final int EMP_RATE_PER_HOUR=20;
+	        public static void main(String[] args)
+	        {
+			int empHrs=0;
+			int empWage=0;
+	                double empCheck=Math.floor(Math.random()*10)%3;
+	                if(empCheck==IS_FULL_TIME)
+	                        empHrs=8;
+	                else if(empCheck==IS_PART_TIME)
+	                        empHrs=4;
 			empWage=empHrs*EMP_RATE_PER_HOUR;
-			totalEmpWage+=empWage;
-		}
-			System.out.println("Total Emp Wage: "+totalEmpWage);
-        }
+			System.out.println("Emp Wage: "+empWage);
+	        }
+
 }
